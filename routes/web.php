e8e4d5bb9aa5f3f33836497078ad6ac58b21cadb
Unknown <franizus@live.com>
2018-02-17 18:45:32 -0500

    changed routing for multiple pages

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,26 @@ Route::get('/', function () {
     return view('index', compact('movies', 'series'));
 })->name('home');
 
-Route::get('/movies/{type}', function ($type) {
-    $api = 'https://api.themoviedb.org/3/movie/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=1&region=us';
+Route::get('/movies/{type}/{page}', function ($type, $page) {
+    $api = 'https://api.themoviedb.org/3/movie/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . $page . '&region=us';
     $movieapi = json_decode(file_get_contents($api), true);
     $movies = $movieapi['results'];
-    return view('movies', compact('movies', 'type'));
+    $api = 'https://api.themoviedb.org/3/movie/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . ($page + 1) . '&region=us';
+    $movieapi = json_decode(file_get_contents($api), true);
+    $movies1 = $movieapi['results'];
+    $movies = array_merge($movies, $movies1); 
+    return view('movies', compact('movies', 'type', 'page'));
 });
 
-Route::get('/series/{type}', function ($type) {
-    $api = 'https://api.themoviedb.org/3/tv/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=1';
+Route::get('/series/{type}/{page}', function ($type, $page) {
+    $api = 'https://api.themoviedb.org/3/tv/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . $page;
     $tvapi = json_decode(file_get_contents($api), true);
     $series = $tvapi['results'];
-    return view('series', compact('series', 'type'));
+    $api = 'https://api.themoviedb.org/3/tv/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . ($page + 1);
+    $tvapi = json_decode(file_get_contents($api), true);
+    $series1 = $tvapi['results'];
+    $series = array_merge($series, $series1);
+    return view('series', compact('series', 'type', 'page'));
 });
 
 Route::get('/movie/{id}', function ($id) {
