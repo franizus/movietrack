adb2f1e77a68550ac1f393c3d44650e11a82022f
Unknown <franizus@live.com>
2018-02-17 18:45:52 -0500

    changed series and movies page layout

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,41 +2,93 @@
 
 @section ('head')
 @extends('layouts.head')
+<style>
+    .col-md-2 {
+        position: relative;
+        min-height: 1px;
+        padding-right: 15px;
+        padding-left: 15px;
+    }
+    
+    .col-md-2 {
+        float: left;
+    }
+    
+    .col-md-2 {
+        width: 16.66666667%;
+    }
+</style>
 @endsection
 
 @section ('content')
-<section class="row">
+<section class="row" style="margin-top: -2em;">
     <div class="col-12">
         <h3 class="mb-4">
             @switch($type)
-                @case('popular')
-                    Populares
-                    @break
-                @case('now_playing')
-                    En Cartelera
-                    @break
-                @case('top_rated')
-                    Mejor Calificadas
-                    @break
-                @case('upcoming')
-                    Proximas
-                    @break
-                @default
+            @case('popular')
+            Populares
+            @break
+            @case('now_playing')
+            En Cartelera
+            @break
+            @case('top_rated')
+            Mejor Calificadas
+            @break
+            @case('upcoming')
+            Proximas
+            @break
+            @default
             @endswitch
         </h3>
     </div>
-    @foreach ($movies as $movie)
-    <div class="col-sm-3 mb-4">
-        <div class="card" style="width:250px">
-            <img class="card-img-top" src="{!! 'https://image.tmdb.org/t/p/w500' . $movie['poster_path'] !!}" alt="Card image" style="width:100%">
-            <div class="card-body">
-                <h4 class="card-title">{{ $movie['title'] }}</h4>
-                <p class="card-text">Rating: {{ $movie['vote_average'] }}</p>
-                <a class="btn btn-primary btn-block" onclick="location.href='{!! '/movie/' . $movie['id'] !!}'">Ver mas</a>
-                <a class="btn btn-success btn-block text-white" onclick="">Seguir</a>
+    <div class="w3_agile_featured_movies">
+        @if ($page > 1)
+        @php
+        $var = 4;
+        $var1 = 28;
+        @endphp
+        @else
+        @php
+        $var = 0;
+        $var1 = 24;
+        @endphp
+        @endif
+        @for ($i = $var; $i < $var1; $i++)
+        <div class="col-md-2 w3l-movie-gride-agile">
+            <a href="{!! '/movie/' . $movies[$i]['id'] !!}" class="hvr-shutter-out-horizontal"><img src="{!! 'https://image.tmdb.org/t/p/w185' . $movies[$i]['poster_path'] !!}" style="width: 200px;height: 300px;" title="{!! $movies[$i]['title'] !!}" class="img-responsive" alt=" " />
+                <div class="w3l-action-icon"><i class="fas fa-plus" aria-hidden="true"></i></div>
+            </a>
+            <div class="mid-1 agileits_w3layouts_mid_1_home">
+                <div class="w3l-movie-text">
+                    <h6><a href="{!! '/movie/' . $movies[$i]['id'] !!}"></a></h6>							
+                </div>
+                <div class="mid-2 agile_mid_2_home">
+                    <p>{{ explode('-', $movies[$i]['release_date'])[0] }}</p>
+                    <div class="block-stars">
+                        <ul class="w3l-ratings">
+                            <li><i class="fas fa-star" aria-hidden="true"></i></li>
+                            <li><i class="fas fa-star" aria-hidden="true"></i></li>
+                            <li><i class="fas fa-star" aria-hidden="true"></i></li>
+                            <li><i class="far fa-star" aria-hidden="true"></i></li>
+                            <li><i class="far fa-star" aria-hidden="true"></i></li>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
             </div>
         </div>
+        @endfor
     </div>
-    @endforeach
+    <div class="col-4 bottom5">
+        @if ($page > 1)
+        <a class="btn btn-primary" href="{!! '/movies/' . $type . '/' . ($page - 1) !!}">Anterior</a>
+        @endif
+    </div>
+    <div class="col-4 bottom5 text-center">
+        <h3>Pagina: {{ $page }}</h3>
+    </div>
+    <div class="col text-right bottom5">
+        <a class="btn btn-primary" href="{!! '/movies/' . $type . '/' . ($page + 1) !!}">Siguiente</a>
+    </div>
 </section>
 @endsection
