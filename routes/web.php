14de784fdfc7edf637ea07cbe1fc3e73f28196fb
Unknown <franizus@live.com>
2018-02-18 03:36:38 -0500

    changed routing file

--- routes/web.php
+++ routes/web.php
@@ -1,77 +1,27 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers;
 
 Route::get('/', function () {
-    $api = 'https://api.themoviedb.org/3/movie/popular?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=1&region=us';
-    $movieapi = json_decode(file_get_contents($api), true);
-    $movies = $movieapi['results'];
-    $api = 'https://api.themoviedb.org/3/tv/popular?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=1';
-    $tvapi = json_decode(file_get_contents($api), true);
-    $series = $tvapi['results'];
+    $movies = Controllers\MoviesController::getMovies('popular', 1);
+    $series = Controllers\SeriesController::getSeries('popular', 1);
     return view('index', compact('movies', 'series'));
 })->name('home');
 
-Route::get('/movies/{type}/{page}', function ($type, $page) {
-    $api = 'https://api.themoviedb.org/3/movie/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . $page . '&region=us';
-    $movieapi = json_decode(file_get_contents($api), true);
-    $movies = $movieapi['results'];
-    $api = 'https://api.themoviedb.org/3/movie/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . ($page + 1) . '&region=us';
-    $movieapi = json_decode(file_get_contents($api), true);
-    $movies1 = $movieapi['results'];
-    $movies = array_merge($movies, $movies1); 
-    return view('movies', compact('movies', 'type', 'page'));
-});
+Route::get('/movies/{type}/{page}', 'MoviesController@index');
+Route::get('/movie/{id}', 'MoviesController@show');
+Route::post('/movie/{id}/comments', 'CommentsController@storeMovie');
+Route::post('/movie/{id}/follow', 'MoviesController@store');
+Route::post('/movie/{id}/unfollow', 'MoviesController@destroy');
 
-Route::get('/series/{type}/{page}', function ($type, $page) {
-    $api = 'https://api.themoviedb.org/3/tv/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . $page;
-    $tvapi = json_decode(file_get_contents($api), true);
-    $series = $tvapi['results'];
-    $api = 'https://api.themoviedb.org/3/tv/' . $type . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES&page=' . ($page + 1);
-    $tvapi = json_decode(file_get_contents($api), true);
-    $series1 = $tvapi['results'];
-    $series = array_merge($series, $series1);
-    return view('series', compact('series', 'type', 'page'));
-});
+Route::get('/series/{type}/{page}', 'SeriesController@index');
+Route::get('/serie/{id}', 'SeriesController@show');
+Route::post('/serie/{id}/comments', 'CommentsController@storeSerie');
+Route::post('/serie/{id}/follow', 'SeriesController@store');
+Route::post('/serie/{id}/unfollow', 'SeriesController@destroy');
+Route::get('/serie/{id}/{season}', 'SeriesController@showSeason');
 
-Route::get('/movie/{id}', function ($id) {
-    $api = 'https://api.themoviedb.org/3/movie/' . $id . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES';
-    $movie = json_decode(file_get_contents($api), true);
-    $apiTrailer = 'https://api.themoviedb.org/3/movie/' . $id . '/videos?api_key=d86068144f769b45826958d1251e8f6d&language=en-US';
-    $trailer = json_decode(file_get_contents($apiTrailer), true);
-    $apiSimilar = 'https://api.themoviedb.org/3/movie/' . $id . '/similar?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES';
-    $similar = json_decode(file_get_contents($apiSimilar), true);
-    if (isset($similar['results'])) {
-        $similar = $similar['results'];
-    } else {
-        $similar = false;
-    }
-    return view('movie', compact('movie', 'trailer', 'similar'));
-});
-
-Route::get('/serie/{id}', function ($id) {
-    $api = 'https://api.themoviedb.org/3/tv/' . $id . '?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES';
-    $serie = json_decode(file_get_contents($api), true);
-    $apiTrailer = 'https://api.themoviedb.org/3/tv/' . $id . '/videos?api_key=d86068144f769b45826958d1251e8f6d&language=en-US';
-    $trailer = json_decode(file_get_contents($apiTrailer), true);
-    $apiSimilar = 'https://api.themoviedb.org/3/tv/' . $id . '/similar?api_key=d86068144f769b45826958d1251e8f6d&language=es-ES';
-    $similar = json_decode(file_get_contents($apiSimilar), true);
-    if (isset($similar['results'])) {
-        $similar = $similar['results'];
-    } else {
-        $similar = false;
-    }
-    return view('serie', compact('serie', 'trailer', 'similar'));
-});
+Route::get('/user/{id}', 'UserController@show');
 
 Route::get('/register', 'RegistrationController@create');
 Route::post('/register', 'RegistrationController@store');
