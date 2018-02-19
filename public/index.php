22cc120cf88aed2b031083c2f83adb543bbb3830
Unknown <alexis.guerrero@epn.edu.ec>
2018-02-19 15:47:03 -0500

    change index

--- public/index.php
+++ public/index.php
@@ -34,6 +34,18 @@ require __DIR__.'/../vendor/autoload.php';
 | the responses back to the browser and delight our users.
 |
 */
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
